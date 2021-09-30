507bf4b1f28fbc8e3f087976bf0cb3b076ef07bf
Cameron Hardy <camhardy99@gmail.com>
2021-09-29 23:07:20 -0500

    Added basic server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
+const {join} = require('path');
 
+const port = 8080;
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(join(__dirname, '../client/build')));
+
+app.listen(port, () => {
+    console.log('Listening on port', port);
+})
