026087a3da5532c36efbda0377d3522b54fa37b6
Cameron Hardy <camhardy99@gmail.com>
2021-10-05 22:55:07 -0500

    Removed no longer needed components and added logic for conditional view based on login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,8 @@
 import "./App.css";
 import { Link, Route, Switch } from "react-router-dom";
 import Home from "./components/Home.jsx";
-import Login from "./components/Login.jsx";
-import NewUser from "./components/NewUser.jsx";
 import { useAuth0 } from "@auth0/auth0-react";
-import { LogoutButton } from "./components/Auth0Button";
+import ProjectView from "./components/ProjectView.jsx";
 
 function App() {
     const { isAuthenticated, user } = useAuth0();
@@ -13,19 +11,15 @@ function App() {
             <Route exact path="/">
                 <Home />
             </Route>
-            <Route path="/newUser">
-                <NewUser />
-            </Route>
             {isAuthenticated ? (
-                <Route path="/loggedin">
-                    <LogoutButton />
-                    <p>{JSON.stringify(user.name)}</p>
+                <Route path="/app">
+                    <ProjectView userInfo={user} />
                 </Route>
             ) : (
-                <div>
-                    <h1>You are not logged in!</h1>
-                    <Link exact to='/'>Return to home page</Link>
-                </div>
+                <>
+                    <Link exact to="/">Home</Link>
+                    <h1>Loading...</h1>
+                </>
             )}
         </Switch>
     );
