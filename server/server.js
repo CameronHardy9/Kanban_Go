5e8931194e4a36f8fa42b3c41d1db3ce5913598d
Cameron Hardy <camhardy99@gmail.com>
2021-10-05 23:03:20 -0500

    Updated port to read from .env and added routes

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
 const {join} = require('path');
+const users = require('../routes/users');
+const projects = require('../routes/projects');
+require('dotenv').config();
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static(join(__dirname, '../client/build')));
 
+app.use('/app', projects);
+app.use('/users', users);
+
 app.listen(port, () => {
     console.log('Listening on port', port);
 })
+
