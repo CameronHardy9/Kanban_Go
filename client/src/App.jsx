7ac626669027d84b9ffc67f69ec2fae02115bc37
Cameron Hardy <camhardy99@gmail.com>
2021-10-06 22:56:53 -0500

    Moved NavBar to be a fixture above react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,36 @@
-import "./App.css";
 import { Link, Route, Switch } from "react-router-dom";
 import Home from "./components/Home.jsx";
 import { useAuth0 } from "@auth0/auth0-react";
 import ProjectView from "./components/ProjectView.jsx";
+import NavBar from "./components/NavBar";
 
 function App() {
     const { isAuthenticated, user } = useAuth0();
     return (
-        <Switch>
-            <Route exact path="/">
-                <Home />
-            </Route>
+        <>
             {isAuthenticated ? (
-                <Route path="/app">
-                    <ProjectView userInfo={user} />
-                </Route>
+                <NavBar isAuthenticated={isAuthenticated} email={user.name} />
             ) : (
-                <>
-                    <Link exact to="/">Home</Link>
-                    <h1>Loading...</h1>
-                </>
+                <NavBar />
             )}
-        </Switch>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                {isAuthenticated ? (
+                    <Route path="/app">
+                        <ProjectView userInfo={user} />
+                    </Route>
+                ) : (
+                    <>
+                        <Link exact to="/">
+                            Home
+                        </Link>
+                        <h1>Loading...</h1>
+                    </>
+                )}
+            </Switch>
+        </>
     );
 }
 
