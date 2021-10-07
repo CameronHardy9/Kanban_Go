3d58843e3ec0e9caa1af65246aa4ae842507773a
Cameron Hardy <camhardy99@gmail.com>
2021-10-07 12:41:38 -0500

    Moved task input data to dedicated component

---
+++ client/src/components/ProjectData.jsx
@@ -0,0 +1,49 @@
+const TestContent = {
+    tasks: {
+        "task1": {
+            id: "task1",
+            title: "Task 1",
+            details: "Some details here",
+        },
+        "task2": {
+            id: "task2",
+            title: "Task 2",
+            details: "Some other details here",
+        },
+        "task3": {
+            id: "task3",
+            title: "Task 3",
+            details: "Some other kinds of details here",
+        },
+        "task4": {
+            id: "task4",
+            title: "Task 4",
+            details: "Last bit of details details here",
+        },
+        "task5": {
+            id: "task5",
+            title: "Task 5",
+            details: "Last bit of details details here",
+        },
+        "task6": {
+            id: "task6",
+            title: "Task 6",
+            details: "Last bit of details details here",
+        },
+    },
+    columns: {
+        "column1": {
+            id: "column1",
+            title: "Title1",
+            taskIds: ["task1", "task2", "task3", "task4"],
+        },
+        "column2": {
+            id: "column2",
+            title: "Title2",
+            taskIds: ["task5", "task6"]
+        }
+    },
+    columnOrder: ["column1", "column2"],
+};
+
+export default TestContent;
