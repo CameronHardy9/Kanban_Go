8d7f9d4132b913b9f231423fe1057c82e0bb9423
Cameron Hardy <camhardy99@gmail.com>
2021-10-06 22:49:51 -0500

    Updated port

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const users = require('../routes/users');
 const projects = require('../routes/projects');
 require('dotenv').config();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 
 const app = express();
 
