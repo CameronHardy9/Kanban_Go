4ab03915d993cb4d64dbf9e27dd290993942f1fd
Cameron Hardy <camhardy99@gmail.com>
2021-10-07 15:28:17 -0500

    Added droppable support and mapping for colomn contents

--- client/src/components/Column.jsx
+++ client/src/components/Column.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import Task from "./Task";
+import { Droppable } from "react-beautiful-dnd";
 
 class Column extends React.Component {
     render() {
         return (
-            
-        )
+            <div className="allColumns">
+                <h2 className="columnTitle">{this.props.column.title}</h2>
+                <Droppable droppableId={this.props.column.id}>
+                    {(provided) => (
+                        <ul
+                            innerRef={provided.innerRef}
+                            {...provided.droppableProps}
+                            className="dragItems"
+                        >
+                            {this.props.tasks.map((task, index) => (
+                                <Task key={task.id} task={task} index={index} />
+                            ))}
+                            {provided.placeholder}
+                        </ul>
+                    )}
+                </Droppable>
+            </div>
+        );
     }
 }
 
