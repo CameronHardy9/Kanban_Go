4d6f5b8e8bb5a64f177b07c944152f5ddfab3b7f
Cameron Hardy <camhardy99@gmail.com>
2021-10-06 22:58:28 -0500

    Removed unused constructor - moved title to navbar - added class name for button container

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,13 +3,9 @@ import {LoginButton, LogoutButton} from "./Auth0Button";
 
 
 class Home extends React.Component {
-    constructor() {
-        super();
-    }
     render() {
         return (
-            <div>
-                <h1>Kanban Go</h1>
+            <div className="homeButtons">
                 <LoginButton />
                 <LogoutButton />
             </div>
