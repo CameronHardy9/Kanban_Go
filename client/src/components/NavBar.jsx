4b428b61163903c927aba50d83a43dcb561e0760
Cameron Hardy <camhardy99@gmail.com>
2021-10-07 22:37:59 -0500

    Updated state with new prop names. Updated title to now link to home.

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { LogoutButton } from "./Auth0Button";
+import { Link } from "react-router-dom";
 
 class NavBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isAuthenticated: this.props.isAuthenticated,
-            email: this.props.email
-        }
+            auth: this.props.auth,
+            email: this.props.email,
+        };
     }
     render() {
-        console.log(this.props.isAuthenticated, this.props.email)
         return (
             <div className="navBar">
-                <h1 className="navTitle">Kanban Go</h1>
-                {this.props.isAuthenticated && (
+                <Link className="navTitle" to="/">
+                    <h1>Kanban Go</h1>
+                </Link>
+                {this.props.auth && (
                     <>
                         <span className="loggedInAs">
                             Logged in as {this.props.email}
