a3db5d658b4d5c971db77cd270db395a2f505835
Cameron Hardy <camhardy99@gmail.com>
2021-10-08 22:20:36 -0500

    removed state and updated variables to new props from parent

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,23 +3,16 @@ import { LogoutButton } from "./Auth0Button";
 import { Link } from "react-router-dom";
 
 class NavBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            auth: this.props.auth,
-            email: this.props.email,
-        };
-    }
     render() {
         return (
             <div className="navBar">
                 <Link className="navTitle" to="/">
                     <h1>Kanban Go</h1>
                 </Link>
-                {this.props.auth && (
+                {this.props.loggedIn && (
                     <>
                         <span className="loggedInAs">
-                            Logged in as {this.props.email}
+                            Logged in as {this.props.userInfo.email}
                         </span>
                         <LogoutButton />
                     </>
