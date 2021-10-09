dc27d48acff7147e4b32c1293b6c0ae3323dddcb
Cameron Hardy <camhardy99@gmail.com>
2021-10-09 12:19:20 -0500

    Refactored to rely on sessionStorage for user login creds and consistancy across refreshes. Removed unneeded state. Added redirects for users already logged in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,43 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "./components/Home.jsx";
 import { useAuth0 } from "@auth0/auth0-react";
 import ProjectView from "./components/ProjectView.jsx";
 import NavBar from "./components/NavBar";
 import React, { useState, useEffect } from "react";
-import storage from "./utils/localStorageInterface.js";
-import useForceUpdate from "./utils/forceUpdate.js";
+import storage from "./utils/sessionStorageInterface.js";
 
 function App() {
     const { user } = useAuth0();
 
-    const [userInfo] = useState(storage.get("KanbanGoAuth"));
-    const [loggedIn] = useState(storage.get("loggedIn"));
+    const [userInfo, setUserInfo] = useState(undefined);
     //console.log(userInfo.sub.split('|')[1])
     useEffect(() => {
         if (user) {
             storage.set("KanbanGoAuth", user);
-            storage.set("loggedIn", true);
         }
-    });
+        setUserInfo(storage.get("KanbanGoAuth"));
+    }, [user]);
     return (
         <>
             {userInfo ? (
-                <NavBar loggedIn={loggedIn} userInfo={storage.get("KanbanGoAuth")} />
+                <NavBar
+                    userInfo={storage.get("KanbanGoAuth")}
+                />
             ) : (
                 <NavBar />
             )}
             <Switch>
                 <Route exact path="/">
-                    <Home />
+                    {userInfo ? <Redirect to="/app" /> : <Home />}
                 </Route>
                 {userInfo ? (
                     <Route path="/app">
                         <ProjectView userInfo={storage.get("KanbanGoAuth")} />
                     </Route>
                 ) : (
-                    <>
+                    <div className="loaderContainer">
                         <div className="loader"></div>
-                    </>
+                    </div>
                 )}
             </Switch>
         </>
