4763f56e8c12153057acd7b00caac77505954815
Cameron Hardy <camhardy99@gmail.com>
2021-10-09 16:31:59 -0500

    Added support for multiple projects in data structure

--- client/src/components/ProjectData.jsx
+++ client/src/components/ProjectData.jsx
@@ -1,79 +1,235 @@
-const TestContent = {
-    tasks: {
-        "task1": {
-            id: "task1",
-            task: "Task 1",
-            details: "Some details here",
+const Projects = {
+    project1: {
+        tasks: {
+            "task1": {
+                id: "task1",
+                task: "Task 1",
+                details: "Some details here",
+            },
+            "task2": {
+                id: "task2",
+                task: "Task 2",
+                details: "Some other details here",
+            },
+            "task3": {
+                id: "task3",
+                task: "Task 3",
+                details: "Some other kinds of details here",
+            },
+            "task4": {
+                id: "task4",
+                task: "Task 4",
+                details: "Last bit of details details here",
+            },
+            "task5": {
+                id: "task5",
+                task: "Task 5",
+                details: "Last bit of details details here",
+            },
+            "task6": {
+                id: "task6",
+                task: "Task 6",
+                details: "Last bit of details details here",
+            },
+            "task7": {
+                id: "task7",
+                task: "Task 7",
+                details: "Last bit of details details here",
+            },
+            "task8": {
+                id: "task8",
+                task: "Task 8",
+                details: "Last bit of details details here",
+            },
+            "task9": {
+                id: "task9",
+                task: "Task 9",
+                details: "Last bit of details details here",
+            },
+            "task10": {
+                id: "task10",
+                task: "Task 10",
+                details: "Last bit of details details here",
+            },
         },
-        "task2": {
-            id: "task2",
-            task: "Task 2",
-            details: "Some other details here",
+        columns: {
+            "column1": {
+                id: "column1",
+                title: "Title1",
+                taskIds: ["task1", "task2", "task3", "task4"],
+            },
+            "column2": {
+                id: "column2",
+                title: "Title2",
+                taskIds: ["task5", "task6"]
+            },
+            "column3": {
+                id: "column3",
+                title: "Title3",
+                taskIds: ["task7", "task8"]
+            },
+            "column4": {
+                id: "column4",
+                title: "Title4",
+                taskIds: ["task9", "task10"]
+            }
         },
-        "task3": {
-            id: "task3",
-            task: "Task 3",
-            details: "Some other kinds of details here",
-        },
-        "task4": {
-            id: "task4",
-            task: "Task 4",
-            details: "Last bit of details details here",
-        },
-        "task5": {
-            id: "task5",
-            task: "Task 5",
-            details: "Last bit of details details here",
-        },
-        "task6": {
-            id: "task6",
-            task: "Task 6",
-            details: "Last bit of details details here",
-        },
-        "task7": {
-            id: "task7",
-            task: "Task 7",
-            details: "Last bit of details details here",
-        },
-        "task8": {
-            id: "task8",
-            task: "Task 8",
-            details: "Last bit of details details here",
-        },
-        "task9": {
-            id: "task9",
-            task: "Task 9",
-            details: "Last bit of details details here",
-        },
-        "task10": {
-            id: "task10",
-            task: "Task 10",
-            details: "Last bit of details details here",
-        },
+        columnOrder: ["column1", "column2", "column3", "column4"]
     },
-    columns: {
-        "column1": {
-            id: "column1",
-            title: "Title1",
-            taskIds: ["task1", "task2", "task3", "task4"],
+    project2: {
+        tasks: {
+            "task1": {
+                id: "task1",
+                task: "Task 1",
+                details: "Some details here",
+            },
+            "task2": {
+                id: "task2",
+                task: "Task 2",
+                details: "Some other details here",
+            },
+            "task3": {
+                id: "task3",
+                task: "Task 3",
+                details: "Some other kinds of details here",
+            },
+            "task4": {
+                id: "task4",
+                task: "Task 4",
+                details: "Last bit of details details here",
+            },
+            "task5": {
+                id: "task5",
+                task: "Task 5",
+                details: "Last bit of details details here",
+            },
+            "task6": {
+                id: "task6",
+                task: "Task 6",
+                details: "Last bit of details details here",
+            },
+            "task7": {
+                id: "task7",
+                task: "Task 7",
+                details: "Last bit of details details here",
+            },
+            "task8": {
+                id: "task8",
+                task: "Task 8",
+                details: "Last bit of details details here",
+            },
+            "task9": {
+                id: "task9",
+                task: "Task 9",
+                details: "Last bit of details details here",
+            },
+            "task10": {
+                id: "task10",
+                task: "Task 10",
+                details: "Last bit of details details here",
+            },
         },
-        "column2": {
-            id: "column2",
-            title: "Title2",
-            taskIds: ["task5", "task6"]
+        columns: {
+            "column1": {
+                id: "column1",
+                title: "Title1",
+                taskIds: ["task1", "task2", "task3", "task4"],
+            },
+            "column2": {
+                id: "column2",
+                title: "Title2",
+                taskIds: ["task5", "task6"]
+            },
+            "column3": {
+                id: "column3",
+                title: "Title3",
+                taskIds: ["task7", "task8"]
+            },
+            "column4": {
+                id: "column4",
+                title: "Title4",
+                taskIds: ["task9", "task10"]
+            }
         },
-        "column3": {
-            id: "column3",
-            title: "Title3",
-            taskIds: ["task7", "task8"]
-        },
-        "column4": {
-            id: "column4",
-            title: "Title4",
-            taskIds: ["task9", "task10"]
-        }
+        columnOrder: ["column1", "column2", "column3", "column4"]
     },
-    columnOrder: ["column1", "column2", "column3", "column4"],
+    project3: {
+        tasks: {
+            "task1": {
+                id: "task1",
+                task: "Task 1",
+                details: "Some details here",
+            },
+            "task2": {
+                id: "task2",
+                task: "Task 2",
+                details: "Some other details here",
+            },
+            "task3": {
+                id: "task3",
+                task: "Task 3",
+                details: "Some other kinds of details here",
+            },
+            "task4": {
+                id: "task4",
+                task: "Task 4",
+                details: "Last bit of details details here",
+            },
+            "task5": {
+                id: "task5",
+                task: "Task 5",
+                details: "Last bit of details details here",
+            },
+            "task6": {
+                id: "task6",
+                task: "Task 6",
+                details: "Last bit of details details here",
+            },
+            "task7": {
+                id: "task7",
+                task: "Task 7",
+                details: "Last bit of details details here",
+            },
+            "task8": {
+                id: "task8",
+                task: "Task 8",
+                details: "Last bit of details details here",
+            },
+            "task9": {
+                id: "task9",
+                task: "Task 9",
+                details: "Last bit of details details here",
+            },
+            "task10": {
+                id: "task10",
+                task: "Task 10",
+                details: "Last bit of details details here",
+            },
+        },
+        columns: {
+            "column1": {
+                id: "column1",
+                title: "Title1",
+                taskIds: ["task1", "task2", "task3", "task4"],
+            },
+            "column2": {
+                id: "column2",
+                title: "Title2",
+                taskIds: ["task5", "task6"]
+            },
+            "column3": {
+                id: "column3",
+                title: "Title3",
+                taskIds: ["task7", "task8"]
+            },
+            "column4": {
+                id: "column4",
+                title: "Title4",
+                taskIds: ["task9", "task10"]
+            }
+        },
+        columnOrder: ["column1", "column2", "column3", "column4"]
+    }
 };
 
-export default TestContent;
+export default Projects;
