cea3b8e6f4fab08a6b22ef90d030c3400163a14d
Cameron Hardy <camhardy99@gmail.com>
2021-10-11 11:46:08 -0500

    Updated test project data to give more difference between examples

--- client/src/components/ProjectData.jsx
+++ client/src/components/ProjectData.jsx
@@ -80,54 +80,34 @@ let Projects = {
         tasks: {
             "task1": {
                 id: "task1",
-                task: "Task 1",
+                task: "Task 11",
                 details: "Some details here",
             },
             "task2": {
                 id: "task2",
-                task: "Task 2",
+                task: "Task 12",
                 details: "Some other details here",
             },
             "task3": {
                 id: "task3",
-                task: "Task 3",
+                task: "Task 13",
                 details: "Some other kinds of details here",
             },
             "task4": {
                 id: "task4",
-                task: "Task 4",
+                task: "Task 14",
                 details: "Last bit of details details here",
             },
             "task5": {
                 id: "task5",
-                task: "Task 5",
+                task: "Task 15",
                 details: "Last bit of details details here",
             },
             "task6": {
                 id: "task6",
-                task: "Task 6",
+                task: "Task 16",
                 details: "Last bit of details details here",
-            },
-            "task7": {
-                id: "task7",
-                task: "Task 7",
-                details: "Last bit of details details here",
-            },
-            "task8": {
-                id: "task8",
-                task: "Task 8",
-                details: "Last bit of details details here",
-            },
-            "task9": {
-                id: "task9",
-                task: "Task 9",
-                details: "Last bit of details details here",
-            },
-            "task10": {
-                id: "task10",
-                task: "Task 10",
-                details: "Last bit of details details here",
-            },
+            }
         },
         columns: {
             "column1": {
@@ -139,72 +119,52 @@ let Projects = {
                 id: "column2",
                 title: "Title2",
                 taskIds: ["task5", "task6"]
-            },
-            "column3": {
-                id: "column3",
-                title: "Title3",
-                taskIds: ["task7", "task8"]
-            },
-            "column4": {
-                id: "column4",
-                title: "Title4",
-                taskIds: ["task9", "task10"]
             }
         },
-        columnOrder: ["column1", "column2", "column3", "column4"]
+        columnOrder: ["column1", "column2"]
     },
     project3: {
         tasks: {
             "task1": {
                 id: "task1",
-                task: "Task 1",
+                task: "Task 21",
                 details: "Some details here",
             },
             "task2": {
                 id: "task2",
-                task: "Task 2",
+                task: "Task 22",
                 details: "Some other details here",
             },
             "task3": {
                 id: "task3",
-                task: "Task 3",
+                task: "Task 23",
                 details: "Some other kinds of details here",
             },
             "task4": {
                 id: "task4",
-                task: "Task 4",
+                task: "Task 24",
                 details: "Last bit of details details here",
             },
             "task5": {
                 id: "task5",
-                task: "Task 5",
+                task: "Task 25",
                 details: "Last bit of details details here",
             },
             "task6": {
                 id: "task6",
-                task: "Task 6",
+                task: "Task 26",
                 details: "Last bit of details details here",
             },
             "task7": {
                 id: "task7",
-                task: "Task 7",
+                task: "Task 27",
                 details: "Last bit of details details here",
             },
             "task8": {
                 id: "task8",
-                task: "Task 8",
+                task: "Task 28",
                 details: "Last bit of details details here",
-            },
-            "task9": {
-                id: "task9",
-                task: "Task 9",
-                details: "Last bit of details details here",
-            },
-            "task10": {
-                id: "task10",
-                task: "Task 10",
-                details: "Last bit of details details here",
-            },
+            }
         },
         columns: {
             "column1": {
@@ -221,14 +181,9 @@ let Projects = {
                 id: "column3",
                 title: "Title3",
                 taskIds: ["task7", "task8"]
-            },
-            "column4": {
-                id: "column4",
-                title: "Title4",
-                taskIds: ["task9", "task10"]
             }
         },
-        columnOrder: ["column1", "column2", "column3", "column4"]
+        columnOrder: ["column1", "column2", "column3"]
     }
 };
 
