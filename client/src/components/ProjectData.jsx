22e9ec4f69ec2c36d53cec1b7c2393ad96d5680c
Cameron Hardy <camhardy99@gmail.com>
2021-10-12 11:29:06 -0500

    Updated test data to have unique values

--- client/src/components/ProjectData.jsx
+++ client/src/components/ProjectData.jsx
@@ -78,112 +78,112 @@ let Projects = {
     },
     project2: {
         tasks: {
-            "task1": {
-                id: "task1",
+            "task11": {
+                id: "task11",
                 task: "Task 11",
                 details: "Some details here",
             },
-            "task2": {
-                id: "task2",
+            "task12": {
+                id: "task12",
                 task: "Task 12",
                 details: "Some other details here",
             },
-            "task3": {
-                id: "task3",
+            "task13": {
+                id: "task13",
                 task: "Task 13",
                 details: "Some other kinds of details here",
             },
-            "task4": {
-                id: "task4",
+            "task14": {
+                id: "task14",
                 task: "Task 14",
                 details: "Last bit of details details here",
             },
-            "task5": {
-                id: "task5",
+            "task15": {
+                id: "task15",
                 task: "Task 15",
                 details: "Last bit of details details here",
             },
-            "task6": {
-                id: "task6",
+            "task16": {
+                id: "task16",
                 task: "Task 16",
                 details: "Last bit of details details here",
             }
         },
         columns: {
-            "column1": {
-                id: "column1",
+            "column5": {
+                id: "column5",
                 title: "Title1",
-                taskIds: ["task1", "task2", "task3", "task4"],
+                taskIds: ["task11", "task12", "task13", "task14"],
             },
-            "column2": {
-                id: "column2",
+            "column6": {
+                id: "column6",
                 title: "Title2",
-                taskIds: ["task5", "task6"]
+                taskIds: ["task15", "task16"]
             }
         },
-        columnOrder: ["column1", "column2"]
+        columnOrder: ["column5", "column6"]
     },
     project3: {
         tasks: {
-            "task1": {
-                id: "task1",
+            "task21": {
+                id: "task21",
                 task: "Task 21",
                 details: "Some details here",
             },
-            "task2": {
+            "task22": {
                 id: "task2",
                 task: "Task 22",
                 details: "Some other details here",
             },
-            "task3": {
-                id: "task3",
+            "task23": {
+                id: "task23",
                 task: "Task 23",
                 details: "Some other kinds of details here",
             },
-            "task4": {
-                id: "task4",
+            "task24": {
+                id: "task24",
                 task: "Task 24",
                 details: "Last bit of details details here",
             },
-            "task5": {
-                id: "task5",
+            "task25": {
+                id: "task25",
                 task: "Task 25",
                 details: "Last bit of details details here",
             },
-            "task6": {
-                id: "task6",
+            "task26": {
+                id: "task26",
                 task: "Task 26",
                 details: "Last bit of details details here",
             },
-            "task7": {
-                id: "task7",
+            "task27": {
+                id: "task27",
                 task: "Task 27",
                 details: "Last bit of details details here",
             },
-            "task8": {
-                id: "task8",
+            "task28": {
+                id: "task28",
                 task: "Task 28",
                 details: "Last bit of details details here",
             }
         },
         columns: {
-            "column1": {
-                id: "column1",
+            "column7": {
+                id: "column7",
                 title: "Title1",
-                taskIds: ["task1", "task2", "task3", "task4"],
+                taskIds: ["task21", "task22", "task23", "task24"],
             },
-            "column2": {
-                id: "column2",
+            "column8": {
+                id: "column8",
                 title: "Title2",
-                taskIds: ["task5", "task6"]
+                taskIds: ["task25", "task26"]
             },
-            "column3": {
-                id: "column3",
+            "column9": {
+                id: "column9",
                 title: "Title3",
-                taskIds: ["task7", "task8"]
+                taskIds: ["task27", "task28"]
             }
         },
-        columnOrder: ["column1", "column2", "column3"]
+        columnOrder: ["column7", "column8", "column9"]
     }
 };
 
