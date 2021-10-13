b32fd9a4c0c211ba6855b47f69bc51b9906d6fa5
Cameron Hardy <camhardy99@gmail.com>
2021-10-13 13:36:38 -0500

    Updated dataConnect to take entire request object.

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/user', (req, res) => {
-    dataConnect(req.method);
+    dataConnect(req);
     res.send("GET from Express")
 })
 
 router.post('/user', (req, res) => {
-    dataConnect(req.method);
+    dataConnect(req);
     res.send("POST Hello from Express")
 })
 
