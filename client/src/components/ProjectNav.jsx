3b5d2ce6921b379e953834d6fc39fa731759466d
Cameron Hardy <camhardy99@gmail.com>
2021-10-12 23:12:41 -0500

    Added start for DB connection. Added button for handling new project entries. Added styling for ProjectNav.

--- client/src/components/ProjectNav.jsx
+++ client/src/components/ProjectNav.jsx
@@ -2,19 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class ProjectNav extends React.Component {
+    handleNewProject = () => {
+        //Insert DB connection
+        //{
+            //Insert empty project structure 
+        //}
+    }
     render() {
         return (
             <div className="projectNav">
                 <details>
                     <summary>My Projects</summary>
-                    <ul>
+                    <ul className="projectNavLinks">
                         {Object.keys(this.props.projects).map((item, index) => {
                             return (
                                 <li key={index}>
-                                    <Link to={`/app/${item}`}>{item}</Link>
+                                    <Link className="projectLink" to={`/app/${item}`}>{item}</Link>
                                 </li>
                             );
                         })}
+                        <li><button id="newProjectButton" onClick={this.handleNewProject}>Add new project</button></li>
                     </ul>
                 </details>
             </div>
