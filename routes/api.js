e329b7c2802a87da4d8c4934f7993a797d0117b0
Cameron Hardy <camhardy99@gmail.com>
2021-10-18 10:05:06 -0500

    Moved database connection logic to each corresponding route.

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,32 @@
-const dataConnect = require('../database/db')
 const express = require('express');
 const router = express.Router();
 
 router.get('/user/:id', async (req, res) => {
-    result = await dataConnect(req.method, req.params.id);
-    res.send(result)
+    try {
+        const result = await req.app.locals.client.db("Kanban_Go").collection("Users").findOne({"_id": req.params.id});
+        res.send(result);
+    } catch (e) {
+        console.error(e);
+    }
 })
 
-router.post('/user/:id', (req, res) => {
-    dataConnect(req.method, req.params.id, req.body);
+router.post('/user/:id', async (req, res) => {
+    try {
+        const result = await req.app.locals.client.db("Kanban_Go").collection("Users").insertOne({"_id": req.params.id, ...req.body});
+        console.log(`Created listing: ${result.insertedId}`); 
+    } catch (e) {
+        console.error(e)
+    }
 })
 
-router.put('/user/:id', (req, res) => {
-    dataConnect(req.method, req.params.id, req.body)
+router.put('/user/:id', async (req, res) => {
+    console.log("PUT")
+    try {
+        const result = req.app.locals.client.db("Kanban_Go").collection("Users").updateOne({"_id": req.params.id}, {$set: req.body});
+        console.log(result);
+    } catch (e) {
+        console.error(e)
+    }
 })
 
 module.exports = router;
