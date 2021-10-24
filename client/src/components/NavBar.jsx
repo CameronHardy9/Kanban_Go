595a4353106fbfb50f61509734195723b66a517f
Cameron Hardy <camhardy99@gmail.com>
2021-10-23 21:51:21 -0500

    Added foundation for save status feature. Visuals are ready - just needs to be fed data.

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -11,8 +11,18 @@ class NavBar extends React.Component {
                 </Link>
                 {this.props.userInfo && (
                     <>
+                        <div className="save">
+                            <span className="saveText">Save status</span>
+                            {true ? (
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
+                            ) : (
+                                <div className="saveLoaderContainer">
+                                    <div className="saveLoader"></div>
+                                </div>
+                            )}
+                        </div>
                         <span className="loggedInAs">
-                            Logged in as {this.props.userInfo.email}
+                            {this.props.userInfo.email}
                         </span>
                         <LogoutButton />
                     </>
