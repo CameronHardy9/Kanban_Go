82d33bc994fa2c2e52b57604b1156f1e5d4ba46e
Cameron Hardy <camhardy99@gmail.com>
2021-10-25 15:41:59 -0500

    Updated save status visual. Updated save status props to pull data from parent state.

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -12,13 +12,18 @@ class NavBar extends React.Component {
                 {this.props.userInfo && (
                     <>
                         <div className="save">
-                            <span className="saveText">Save status</span>
-                            {true ? (
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
+                            {this.props.contentSaved ? (
+                                <>
+                                    <span className="saveText">Saved</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
+                                </>
                             ) : (
-                                <div className="saveLoaderContainer">
-                                    <div className="saveLoader"></div>
-                                </div>
+                                <>
+                                    <span className="saveText">Saving...</span>
+                                    <div className="saveLoaderContainer">
+                                        <div className="saveLoader"></div>
+                                    </div>
+                                </>
                             )}
                         </div>
                         <span className="loggedInAs">
