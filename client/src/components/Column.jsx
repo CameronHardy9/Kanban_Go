f5dc89b4806d12525d622c74682084341b7e79e1
Cameron Hardy <camhardy99@gmail.com>
2021-10-28 12:06:16 -0500

    Added column delete feature

--- client/src/components/Column.jsx
+++ client/src/components/Column.jsx
@@ -7,6 +7,9 @@ class Column extends React.Component {
         return (
             <>
                 <h2 className="columnTitle" contentEditable suppressContentEditableWarning onBlur={(e) => this.props.handleColumnContentChange(e, this.props.column.id)}>{this.props.column.title}</h2>
+                <button className="buttons deleteColumn" onClick={() => this.props.handleDeleteColumn(this.props.column.id)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                </button>
                 <Droppable droppableId={this.props.column.id}>
                     {(provided) => (
                         <ul
