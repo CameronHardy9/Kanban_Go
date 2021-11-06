d7ecf1031c6d588c774c6bf89ce6e5296f04214c
Cameron Hardy <camhardy99@gmail.com>
2021-11-05 23:14:24 -0500

    Fixed incorrect React list key locations

--- client/src/components/ProjectView.jsx
+++ client/src/components/ProjectView.jsx
@@ -329,8 +329,8 @@ class ProjectView extends React.Component {
                                                 const column = project.columns[columnId];
                                                 const tasks = column.taskIds.map((taskId) => project.tasks[taskId]);
                                                 return(
-                                                <div className="allColumns">
-                                                    <Column key={columnId} column={column} tasks={tasks} handleDeleteColumn={this.handleDeleteColumn} handleDeleteTask={this.handleDeleteTask} handleColumnContentChange={this.handleColumnContentChange} handleTaskContentChange={this.handleTaskContentChange}/>
+                                                <div key={columnId} className="allColumns">
+                                                    <Column column={column} tasks={tasks} handleDeleteColumn={this.handleDeleteColumn} handleDeleteTask={this.handleDeleteTask} handleColumnContentChange={this.handleColumnContentChange} handleTaskContentChange={this.handleTaskContentChange}/>
                                                     <button className="buttons" onClick={() => this.handleAddTask(column.id)}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-plus-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg></button>
                                                 </div>
                                                 )
