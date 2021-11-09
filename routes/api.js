3485801db39a39247e7e06ac39799cbd9d07e1fb
Cameron Hardy <camhardy99@gmail.com>
2021-11-09 11:27:07 -0600

    Updated api routes to handle res - fixed db update issue

--- routes/api.js
+++ routes/api.js
@@ -13,17 +13,16 @@ router.get('/user/:id', async (req, res) => {
 router.post('/user/:id', async (req, res) => {
     try {
         const result = await req.app.locals.client.db("Kanban_Go").collection("Users").insertOne({"_id": req.params.id, ...req.body});
-        console.log(`Created listing: ${result.insertedId}`); 
+        res.send(`Created listing: ${result.insertedId}`); 
     } catch (e) {
         console.error(e)
     }
 })
 
 router.put('/user/:id', async (req, res) => {
-    console.log("PUT")
     try {
         const result = req.app.locals.client.db("Kanban_Go").collection("Users").updateOne({"_id": req.params.id}, {$set: req.body});
-        console.log(result);
+        res.send(result);
     } catch (e) {
         console.error(e)
     }
