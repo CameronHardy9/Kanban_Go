37db681f00ae8addd371de2a11a3e541d56c0d8c
Cameron Hardy <camhardy99@gmail.com>
2021-12-24 14:35:05 -0600

    Changed server file paths

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.static(join(__dirname, 'client/build')));
+app.use(express.static(join(__dirname, '../client/build')));
 
 app.use('/app', projects);
 app.use('/api', api);
