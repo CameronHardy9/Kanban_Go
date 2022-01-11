0ce20ef45c652ab7ae595d822640d9f5942cbf6e
Cameron Hardy <camhardy99@gmail.com>
2022-01-10 22:22:39 -0600

    Deleted unused components

--- server/server.js
+++ server/server.js
@@ -1,24 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
-const helmet = require('helmet');
 const cors = require('cors')
-const {join} = require('path');
 const { MongoClient } = require('mongodb');
 const api = require('./routes/api');
-const projects = require('./routes/projects');
 require('dotenv').config();
 
 const port = process.env.PORT || 8080;
 
 const app = express();
 
-//app.use(helmet());
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.static(join(__dirname, '../client/build')));
 
-app.use('/app', projects);
 app.use('/api', api);
 
 MongoClient.connect(process.env.MONGO_DB_URI, {promiseLibrary: Promise}, (err, client) => {
